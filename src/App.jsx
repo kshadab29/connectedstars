98eb95b77749fa5ea0393451baf4d29c98a608e1
Shadab <contactshadab29@gmail.com>
2024-06-27 21:24:30 +0530

    text size increased

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-     <p className="text-gray-100 bg-black text-2xl">Welcome to Connected Stars</p>
+     <p className="text-gray-100 bg-black text-3xl">Welcome to Connected Stars</p>
     </>
   )
 }
